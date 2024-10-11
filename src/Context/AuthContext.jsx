43f22591c8783f5d307a76282d60a3470696fd0c
Mhizdhee <olaniyandorcas91@gmail.com>
2024-10-10 18:01:27 -0700

    fixes

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,82 +1,11 @@
-import { createContext, useState, useEffect } from "react";
-import authApi from "../api/authApi";
-
-const AuthContext = createContext();
-
-export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const login = async () => {
-    try {
-      const data = await authApi();
-
-      setAuth({
-        token: data.jwtToken,
-        tokenExpiry: data.tokenExpiry,
-        username: data.username,
-      });
-    } catch (error) {
-      setError('Internal Server Error. Please try again later.');
-
-      console.error("Login error:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const loadAuthFromLocalStorage = () => {
-    const jwtToken = localStorage.getItem("authToken");
-    const tokenExpiry = localStorage.getItem("tokenExpiry");
-    const username = localStorage.getItem("username");
-
-    if (jwtToken && tokenExpiry && username) {
-      const currentTime = new Date().getTime();
-      if (currentTime < tokenExpiry) {
-        // Token is still valid
-        setAuth({
-          token: jwtToken,
-          tokenExpiry,
-          username,
-        });
-      } else {
-        // Token has expired
-        localStorage.clear(); // Clear the token from local storage
-        login(); // Fetch new token
-      }
-    } else {
-      // No token, perform login
-      login();
-    }
-  };
-
-  useEffect(() => {
-    loadAuthFromLocalStorage();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <AuthContext.Provider value={{ auth }}>{children}</AuthContext.Provider>
-  );
-};
-
-export default AuthContext;
-
-
-
-
-
-// import { createContext, useState, useEffect, useContext } from "react";
+// import { createContext, useState, useEffect } from "react";
 // import authApi from "../api/authApi";
 
 // const AuthContext = createContext();
 
-
 // export const AuthProvider = ({ children }) => {
 //   const [auth, setAuth] = useState(null);
+//   const [loading, setLoading] = useState(false);
 
 //   const login = async () => {
 //     try {
@@ -88,19 +17,90 @@ export default AuthContext;
 //         username: data.username,
 //       });
 //     } catch (error) {
+//       setError('Internal Server Error. Please try again later.');
+
 //       console.error("Login error:", error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   const loadAuthFromLocalStorage = () => {
+//     const jwtToken = localStorage.getItem("authToken");
+//     const tokenExpiry = localStorage.getItem("tokenExpiry");
+//     const username = localStorage.getItem("username");
+
+//     if (jwtToken && tokenExpiry && username) {
+//       const currentTime = new Date().getTime();
+//       if (currentTime < tokenExpiry) {
+//         // Token is still valid
+//         setAuth({
+//           token: jwtToken,
+//           tokenExpiry,
+//           username,
+//         });
+//       } else {
+//         // Token has expired
+//         localStorage.clear(); // Clear the token from local storage
+//         login(); // Fetch new token
+//       }
+//     } else {
+//       // No token, perform login
+//       login();
 //     }
 //   };
 
 //   useEffect(() => {
-//     login();
+//     loadAuthFromLocalStorage();
 //   }, []);
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
 
 //   return (
 //     <AuthContext.Provider value={{ auth }}>{children}</AuthContext.Provider>
 //   );
 // };
 
-
-
 // export default AuthContext;
+
+
+
+
+
+import { createContext, useState, useEffect, useContext } from "react";
+import authApi from "../api/authApi";
+
+const AuthContext = createContext();
+
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(null);
+
+  const login = async () => {
+    try {
+      const data = await authApi();
+
+      setAuth({
+        token: data.jwtToken,
+        tokenExpiry: data.tokenExpiry,
+        username: data.username,
+      });
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
+  useEffect(() => {
+    login();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ auth }}>{children}</AuthContext.Provider>
+  );
+};
+
+
+
+export default AuthContext;
